be4a8af112fc8169a4f9f3d6c168a0510cb59522
zzpengg <cfps91152@gmail.com>
2017-09-26 09:11:39 +0800

    vue starbase router practice

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<template>
+    <div id="app">
+        <h3>Starbase Router</h3>
+        <h4>
+            <router-link class="link" to="/data/people">Characters</router-link> | 
+            <router-link class="link" to="/data/planets">Planets</router-link>
+        </h4>
+        <router-view></router-view>
+    </div>
+</template>
+
+<script>
+export default {
+    
+}
+</script>
---
+++ src/components/Data.vue
@@ -0,0 +1,50 @@
+<template>
+    <div class="col-md-12">
+        <Item 
+            v-for="(item, index) in items"
+            v-bind:key="index"
+            :passed-item="item"
+            :type="type"
+        />
+    </div>
+</template>
+
+<script>
+import Item from './Item.vue';
+
+export default {
+    data() {
+        return {
+            type: this.$route.params.type,
+            items: []
+        }
+    },
+    watch: {
+        '$route': 'fetchItems'
+    },
+    methods: {
+        fetchItems() {
+            this.type = this.$route.params.type;
+            let initial_ids = [1, 13, 14];
+            this.items = [];
+            for(let i in initial_ids){
+                let id = initial_ids[i];
+                fetch(`https://swapi.co/api/${this.type}/${id}`, {
+                    method: 'GET'
+                })
+                    .then(response => response.json())
+                    .then(json => this.items.push(json));
+
+            }
+            
+        }
+    },
+    created() {
+        this.fetchItems();
+    },
+    components: {
+        Item
+    }
+}
+</script>
+
